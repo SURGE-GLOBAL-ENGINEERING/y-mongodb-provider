7c192b86662394dec5237bed2d514a4416c3a51e
Janith Gamage <gamage1995@gmail.com>
2024-05-17 18:56:58 +0530

    chore/main Update deployment scripts

--- rollup.config.js
+++ rollup.config.js
@@ -40,13 +40,13 @@ export default [
 		input: './src/y-mongodb.js',
 		output: {
 			name: 'Y',
-			file: 'dist/y-mongodb.mjs',
-			format: 'esm',
+			file: 'dist/y-mongodb.cjs',
+			format: 'cjs',
 			sourcemap: true,
 			paths: (path) => {
 				if (/^lib0\//.test(path)) {
 					// return `lib0/dist/${path.slice(5, -3)}.cjs
-					return `lib0/dist/${path.slice(5)}.mjs`;
+					return `lib0/dist/${path.slice(5)}.cjs`;
 				}
 				return path;
 			},
